3f5b311c0d718cd598836c8ee58e4d8a984f2a32
张伟 <jtyoui@qq.com>
2022-01-19 14:56:07 +0800

    增加对返回参数的二次封装

--- dao/bind.go
+++ dao/bind.go
@@ -5,6 +5,7 @@
 package dao
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/jtyoui/ginRoute/dao/post"
 	"github.com/jtyoui/ginRoute/tool"
@@ -34,28 +35,64 @@ func (b *BindHandler) BindParams(hrm web.HRM) func(context *gin.Context) {
 
 			// 判断参数是不是*gin.Context
 			if p.Kind() == reflect.Ptr && rp.Name() == "Context" {
-				params[i] = reflect.ValueOf(c)
+				params[i] = reflect.ValueOf(c) // 如果是gin的上下文直接返回
 				continue
 			}
 
-			var value reflect.Value
-			var err error
+			var (
+				value reflect.Value
+				err   error
+			)
+
 			switch hrm {
 			case web.GET, web.DELETE:
-				value, err = GetBind(c, rp, b.Params[i])
+				value, err = GetBind(c, rp, b.Params[i]) // 走的get绑定
 			case web.POST, web.PUT:
-				value, err = post.JsonBind(c, rp)
+				value, err = post.JsonBind(c, rp) // 走的post绑定。现在默认只有json格式
 			}
 			if err != nil {
-				c.JSON(http.StatusOK, web.NewError(err))
+				response(c, web.NewError(err)) // 绑定失败
 				return
 			}
-			if p.Kind() != reflect.Ptr {
+			if p.Kind() != reflect.Ptr { // 判断绑定是不是指针类型
 				value = value.Elem()
 			}
 			params[i] = value
 		}
-		b.F.Call(params)
+		values := b.F.Call(params) // 回调注册函数。获取返回值
+		if values != nil {
+			resultByData(c, values)
+		}
 	}
 	return f
+}
+
+// 返回响应
+func response(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, data)
+}
+
+// 更加函数返回的数据来返回结果
+func resultByData(c *gin.Context, values []reflect.Value) {
+	data := values[0].Interface() // 获取第一个参数
+	length := len(values)         // 获取长度
+
+	if length == 1 {
+		response(c, data)
+	} else if length == 2 {
+		isErr := values[1].Interface()
+		if isErr == nil { // 如果第二个参数是nil。返回第一个参数内容
+			response(c, data)
+		} else {
+			if err, ok := isErr.(error); ok {
+				response(c, web.NewError(err))
+			} else {
+				err := errors.New("第二个出参必须是error类型")
+				response(c, web.NewError(err))
+			}
+		}
+	} else {
+		err := errors.New("出参的长度个数不能大于2")
+		response(c, web.NewError(err))
+	}
 }
